db1af50c23ce7dcde2e2699ecc4a61742b0950f0
12Belete <154913581+12Belete@users.noreply.github.com>
2026-07-31 18:51:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+function login(){
+    alert("BEELKO Login is coming soon!");
+}
 
+function signup(){
+    alert("BEELKO Sign Up is coming soon!");
+}
+
+function wallet(){
+    alert("Wallet feature is under development.");
+}
+
+function liveRooms(){
+    alert("Live Audio Rooms coming soon.");
+}
+
+function chat(){
+    alert("Chat feature coming soon.");
+}
