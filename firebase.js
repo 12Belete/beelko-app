2a72d5981de1e1312cd06ce37e9386b956b776ea
12Belete <154913581+12Belete@users.noreply.github.com>
2026-07-31 18:53:51 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// BEELKO Firebase Configuration
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "beelko-7dcaa.firebaseapp.com",
+  projectId: "beelko-7dcaa",
+  storageBucket: "beelko-7dcaa.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Firebase SDK code yeroo itti aanu asitti dabalna.
+console.log("BEELKO Firebase Ready");
