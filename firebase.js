2fc5638de417a2c755d6d3e58add0c9105c1363a
12Belete <154913581+12Belete@users.noreply.github.com>
2026-08-01 10:01:56 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,22 @@
-// Import Firebase
+// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Firebase Configuration
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyDiM1RFp-yWr9luVLJslNJYiZOhsiwLLO4",
   authDomain: "beelko-7dcaa.firebaseapp.com",
   projectId: "beelko-7dcaa",
   storageBucket: "beelko-7dcaa.firebasestorage.app",
   messagingSenderId: "757679525639",
-  appId: "1:757679525639:web:faceb6756f2ab687de7fa2",
-  measurementId: "G-7YPRWT675Q"
+  appId: "1:757679525639:web:faceb6756f2ab687de7fa2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-console.log("✅ BEELKO Firebase Connected");
-export { auth, db };
+// Qorannoo qofa
+window.auth = auth;
+
+console.log("BEELKO Firebase Connected");
